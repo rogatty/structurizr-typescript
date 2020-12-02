e410b2df25f2e2b4171cb5e9ec1714540824200a
Igor Rogatty <7030884+rogatty@users.noreply.github.com>
2020-12-02 15:43:54 +0100

    Allow Person to be used 😬

--- src/core/model/staticStructureElement.ts
+++ src/core/model/staticStructureElement.ts
@@ -4,10 +4,11 @@ import { Relationship } from "./relationship";
 import { InteractionStyle } from "./interactionStyle";
 import { Container } from "./container";
 import {Component} from "./component";
+import { Person } from "./person";
 
 export abstract class StaticStructureElement extends Element {
 
-    public uses(destination: SoftwareSystem | Container | Component, description: string, technology?: string, interactionStyle = InteractionStyle.Synchronous): Relationship | null {
+    public uses(destination: SoftwareSystem | Container | Component | Person, description: string, technology?: string, interactionStyle = InteractionStyle.Synchronous): Relationship | null {
         return this.model.addRelationship(this, destination, description, technology, interactionStyle);
     }
 
